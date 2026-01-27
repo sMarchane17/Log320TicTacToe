6ef812113e97e7b6a4470291e364db4652bc0a84
User <user@example.com>
2026-01-27 15:54:26 -0500

    Définition des règles du jeux

--- Board.java
+++ Board.java
@@ -10,6 +10,14 @@ class Board
 
     // Ne pas changer la signature de cette méthode
     public Board() {
+        board = new Mark[3][3];
+
+        //Remplir le tableau vide
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                board[i][j] = Mark.EMPTY;
+            }
+        }
 
     }
 
@@ -18,16 +26,57 @@ class Board
     //
     // Ne pas changer la signature de cette méthode
     public void play(Move m, Mark mark){
-
+        board[m.getRow()][m.getCol()] = mark;
 
     }
 
+    private boolean hasWon(Mark mark){
+        //Vérifier les lignes
+        for(int i = 0; i < 3; i++){
+            if(board[i][0] == mark && board[i][1] == mark && board[i][2] == mark){
+                return true;
+            }
+        }
+
+        //Vérifier les colonnes
+        for(int j = 0; j < 3; j++){
+            if(board[0][j] == mark && board[1][j] == mark && board[2][j] == mark){
+                return true;
+            }
+        }
+
+        //Vérifier les diagonales
+        if(board[0][0] == mark && board[1][1] == mark && board[2][2] == mark){
+            return true;
+        }
+        if(board[0][2] == mark && board[1][1] == mark && board[2][0] == mark){
+            return true;
+        }
+
+        return false;
+    }
 
     // retourne  100 pour une victoire
     //          -100 pour une défaite
     //           0   pour un match nul
     // Ne pas changer la signature de cette méthode
     public int evaluate(Mark mark){
-
+        if(mark == Mark.X){
+            if(hasWon(Mark.X)){
+                return 100;
+            }
+            else if(hasWon(Mark.O)){
+                return -100;
+            }
+        }
+        else{
+            if(hasWon(Mark.O)){
+                return 100;
+            }
+            else if(hasWon(Mark.X)){
+                return -100;
+            }
+        }
+        return 0;
     }
 }
